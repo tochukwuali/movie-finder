a77d5365f87ac09adfd349dfa313bccda8a95bb1
tcee42 <toochukwuali@gmail.com>
2020-05-19 18:16:04 +0100

    Addded movie and movieItem components

---
+++ movies.js
@@ -0,0 +1,36 @@
+ export const state = {
+     movies: [
+       {
+         id: 1,
+         title: "Harry Potter",
+         year: "2005",
+         rating: 4.5,
+         genre: "thriller",
+         imgUrl: "../images/Harry-Potter.jpg"
+       },
+       {
+         id: 2,
+         title: "How I met your mother ",
+         year: "2005",
+         rating: 4.6,
+         genre: "sitcom",
+         imgUrl: "../images/How-I-Met-Your-Mother.jpg"
+       },
+       {
+         id: 3,
+         title: "Friends",
+         year: "1994",
+         rating: 4.5,
+         genre: "sitcom",
+         imgUrl: "../images/Friends.jpg"
+       },
+       {
+         id: 4,
+         title: "Silicon Valley ",
+         year: "2014",
+         rating: 4.7,
+         genre: "comedy",
+         imgUrl: "../images/Silicon-Valley.jpg"
+       }
+     ]
+   };
