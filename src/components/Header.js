94b1f069cd0799b6680b659f06721911f31f04b3
tcee42 <toochukwuali@gmail.com>
2020-05-19 15:15:09 +0100

    Added a header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h3> Movie Cards</h3>
+            <p>A simple and visually appealing display of your favourite movies</p>
+        </div>
+    )
+}
+
+export default Header
